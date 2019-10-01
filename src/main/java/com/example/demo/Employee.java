4bc91ddb497285d3f1854a56c3c4a4637c9534bc
Mkawera1 <GBTC441003UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-01 16:28:10 -0400

    Exercise_203

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Employee {
+    private String fName;
+    private String lName;
+    private int SSN;
+    private int DOB;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public int getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(int SSN) {
+        this.SSN = SSN;
+    }
+
+    public int getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(int DOB) {
+        this.DOB = DOB;
+    }
+}
